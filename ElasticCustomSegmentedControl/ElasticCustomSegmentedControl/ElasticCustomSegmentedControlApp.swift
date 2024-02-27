43c157a47696b64bcc3948fa508e20de12df434d
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-27 10:59:49 +0200

    init project

---
+++ ElasticCustomSegmentedControl/ElasticCustomSegmentedControl/ElasticCustomSegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElasticCustomSegmentedControlApp.swift
+//  ElasticCustomSegmentedControl
+//
+//  Created by OLEKSANDR ISAIEV on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ElasticCustomSegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
