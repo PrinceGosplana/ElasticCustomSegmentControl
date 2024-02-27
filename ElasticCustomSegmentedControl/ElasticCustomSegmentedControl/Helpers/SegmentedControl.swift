330154f8447898a7111b42f6d32d0528079fb3c4
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-27 11:03:43 +0200

    add empty file

---
+++ ElasticCustomSegmentedControl/ElasticCustomSegmentedControl/Helpers/SegmentedControl.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControl.swift
+//  ElasticCustomSegmentedControl
+//
+//  Created by OLEKSANDR ISAIEV on 27.02.2024.
+//
+
+import SwiftUI
+
+struct SegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegmentedControl()
+}
